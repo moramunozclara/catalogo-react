2893e466dc9311fe917493448cce6bf48f420e16
Clara Mora <moramunozclara@alumnos.cei.es>
2024-07-25 08:02:53 +0200

    fix

---
+++ src/pages/productospensados.jsx
@@ -0,0 +1,80 @@
+
+
+// @font-face {
+//     font-family: 'GohuFont';
+//     src: url('/src/assets/fonts/GohuFont11NerdFontMono-Regular.ttf') format('truetype');
+//     font-weight: normal;
+//     font-style: normal;
+// }
+
+// body {
+//     font-family: 'GohuFont', sans-serif;
+// }
+
+
+const productospensados = () => {
+    return ( 
+        <div>
+
+
+
+1. Vertidos navales prestigiosos
+        <li>Sobornos a autoridades medioambientales.</li>
+        <li>Manipulación de medios de comunicación para desviar la atención.</li>
+        <li>Compensaciones a las víctimas para evitar demandas.</li>
+        <li>Eliminación de investigadores</li>
+
+
+        6. "Isla Privada de Silencio"
+        Precio: €600,000,000
+
+        Descripción:
+        Evita que cualquier escándalo sexual salga a la luz con este paquete exclusivo inspirado en la famosa mansión de un conocido magnate. Incluye:
+        <li>Sobornos a las víctimas, autoridades locales e internacionales.</li>
+        <li>Manipulación de registros de vuelo y viajes.</li>
+        <li>Compra de testimonios y silenciamiento de testigos.</li>
+        <li>Creación de fundaciones caritativas</li>
+        <li>Represión y control de medios para evitar filtraciones</li>
+
+
+
+        8. "Moda Sin Huella Élite"
+        Precio: €1,200,000,000
+
+        Descripción:
+        Protege tu empresa de ropa multimillonaria de cualquier controversia relacionada con condiciones laborales abusivas y evasión fiscal. Este paquete exclusivo incluye:
+
+            Donaciones estratégicas de material médico y otras caridades en el país de evasión.
+            Campañas de relaciones públicas que destacan tus contribuciones sociales.
+            Sobornos a inspectores laborales y fiscales en países tercermundistas.
+            Silenciamiento de periodistas y activistas críticos.
+
+
+
+        11. "Entrena a tu terrorista"
+        Precio: €2,500,000,000
+
+        Descripción:
+        Proporciona entrenamiento y apoyo militar a grupos encubiertos sin dejar rastros. 
+        Este paquete exclusivo incluye:
+
+        Entrenamiento militar avanzado:
+        Entrena a cientos de combatientes con técnicas de guerrilla y operaciones especiales.
+        Suministro de armamento y equipo militar de última generación.
+        Cobertura diplomática:
+        Protege tus operaciones con apoyo de embajadas y acuerdos internacionales.
+        Manipulación de medios:
+        Controla la narrativa mediática para justificar o encubrir tus acciones.
+        Fondos ocultos y transferencias bancarias encubiertas para financiar operaciones.
+        Logística y transporte de personal y equipos a zonas de conflicto.
+        Neutralización de periodistas y activistas
+            
+
+
+
+
+        </div>
+     );
+}
+ 
+export default productospensados;
